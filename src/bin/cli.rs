c3aae8be3908b395b726e5b05fba6ad5f2a2b007
Andrew Radev <andrey.radev@gmail.com>
2022-01-03 12:19:50 +0200

    Example with cmdline handling

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::ffi::OsString;
+
 // use gtk4 as gtk;
 use gtk::prelude::*;
 use gtk::{
@@ -9,18 +12,28 @@ use gtk::{
     Orientation,
     ScrolledWindowBuilder,
 };
+use gio::ApplicationFlags;
 use glib::clone;
 
 fn main() {
-    let application = Application::builder()
-        .application_id("com.andrewradev.ClickExample")
-        .build();
+    let application = Application::builder().
+        application_id("com.andrewradev.ClickExample").
+        flags(ApplicationFlags::HANDLES_OPEN | ApplicationFlags::HANDLES_COMMAND_LINE).
+        build();
 
-    application.connect_activate(|app| build_ui(app));
-    application.run();
+    application.connect_command_line(move |app, cmdline| {
+        build_ui(&app, cmdline.arguments());
+        0
+    });
+
+    application.run_with_args(&env::args().collect::<Vec<_>>());
 }
 
-fn build_ui(app: &Application) {
+fn build_ui(app: &Application, arguments: Vec<OsString>) {
+    let button_label = arguments.get(1).
+        and_then(|arg| arg.clone().into_string().ok()).
+        unwrap_or_else(|| String::from("Add click entry"));
+
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Click Example")
@@ -29,23 +42,11 @@ fn build_ui(app: &Application) {
         .build();
 
     let outer_container = GtkBox::new(Orientation::Vertical, 10);
-    let button = Button::with_label("Add click entry");
-
-    // GTK_DEBUG=interactive
-    // let scrolled_window = gtk::ScrolledWindow::new();
-
+    let button = Button::with_label(&button_label);
     let scrolled_window = ScrolledWindowBuilder::new().
         vexpand(true).
         build();
-
     let entry_container = GtkBox::new(Orientation::Vertical, 10);
-
-    // button.connect_clicked(move |_| {
-    //     let info = format!("Clicked on the button at {}", ::chrono::Local::now());
-    //     let label = Label::new(Some(&info));
-    //
-    //     entry_container.append(&label);
-    // });
 
     button.connect_clicked(clone!(@strong entry_container => move |_| {
         let info = format!("Clicked on the button at {}", ::chrono::Local::now());
